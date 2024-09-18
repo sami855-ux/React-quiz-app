224030ce410a47eab3bc8f87ff582e6a002f67f1
sami855-ux <samitale56@outlook.com>
2024-09-18 13:35:50 +0500

    handeling the context to display the questions

--- src/Context/DataProvider.jsx
+++ src/Context/DataProvider.jsx
@@ -8,6 +8,8 @@ const DataContext = createContext()
 const initialState = {
   questions: questionData,
   status: "ready",
+  index: 0,
+  answer: null,
 }
 
 const reducer = (state, action) => {
@@ -17,18 +19,46 @@ const reducer = (state, action) => {
         ...state,
         status: "active",
       }
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      }
+    case "setAnswer":
+      return {
+        ...state,
+        answer: action.payload,
+      }
     default:
       throw new Error("unknown action is applied")
   }
 }
 
 const DataProvider = ({ children }) => {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState)
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  )
+
+  const numofQuestions = questions.length
+  const maxPoints = questions.reduce((perv, curr) => perv + curr.points, 0)
+  const currQuestion = questions[index]
+
+  function handelNext() {
+    dispatch({ type: "nextQuestion" })
+  }
 
   const value = {
     dispatch,
     questions,
     status,
+    numofQuestions,
+    maxPoints,
+    currQuestion,
+    onNext: handelNext,
+    answer,
+    index,
   }
 
   return <DataContext.Provider value={value}>{children}</DataContext.Provider>
